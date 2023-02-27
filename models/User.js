d41da81f5aa195d7b2714863d9960b6423b77ec3
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-27 21:30:01 +0530

    made mongoose schema for user details

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  domain: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  // image: {
+  //   data: Buffer,
+  //   contentType: Image,
+  // },
+});
+
+const Users = mongoose.model("users", UserSchema);
+module.exports = Users;
